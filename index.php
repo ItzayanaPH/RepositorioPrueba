68ecafc8e39682e0ad96fff0bd19350423c59513
ItzayanaPH <itzayana.ipn.escom@hotmail.com>
2016-08-28 23:56:19 -0500

    Archivo PHP
    Hola PHP

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+         echo "<p>Hola Mundo</p>";
+        ?>
+    </body>
+</html>
